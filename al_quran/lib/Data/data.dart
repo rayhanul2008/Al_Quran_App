4d22ca7f0daa2df964c22ea2e2b0ff10f3195622
Md Rayhanul Islam <73143090+rayhanul2008@users.noreply.github.com>
2023-11-05 20:51:05 +0600

    update

--- al_quran/lib/Data/data.dart
+++ al_quran/lib/Data/data.dart
@@ -1,53 +1,55 @@
 List quranData = [
   {
+    // English- Yusuf Ali
+    // Ayat - Uthmani Hafs
     "Surah No": 1,
     "Name": "সূরা আল ফাতিহা",
     "Ayats": [
       {
-        "Ayat": "بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ",
+        "Ayat": "﻿بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ",
         "Bangla Translation":
             "শুরু করছি আল্লাহর নামে যিনি পরম করুণাময়, অতি দয়ালু।",
         "English Translation":
-            "In the Name of Allah, the Most Beneficent, the Most Merciful"
-      }, // Ayat end
+            "In the name of God, Most Gracious, Most Merciful"
+      },
       {
         "Ayat": "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ",
         "Bangla Translation":
             "যাবতীয় প্রশংসা আল্লাহ তা’আলার যিনি সকল সৃষ্টি জগতের পালনকর্তা।",
         "English Translation":
-            "Praise be to Allah, the Cherisher and Sustainer of the worlds;"
-      }, // Ayat End
+            "Praise be to God, the Cherisher and Sustainer of the worlds"
+      },
       {
         "Ayat": "الرَّحْمَـٰنِ الرَّحِيمِ",
         "Bangla Translation": "যিনি নিতান্ত মেহেরবান ও দয়ালু।",
-        "English Translation": "Most Gracious, Most Merciful;"
-      }, // Ayat End
+        "English Translation": "Most Gracious, Most Merciful"
+      },
       {
         "Ayat": "مَالِكِ يَوْمِ الدِّينِ",
         "Bangla Translation": "যিনি বিচার দিনের মালিক।",
-        "English Translation": "Master of the Day of Judgment."
-      }, // Ayat End
+        "English Translation": "Master of the Day of Judgment"
+      },
       {
         "Ayat": "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ",
         "Bangla Translation":
             "আমরা একমাত্র তোমারই ইবাদত করি এবং শুধুমাত্র তোমারই সাহায্য প্রার্থনা করি।",
-        "English Translation": "Thee do we worship, and Thine aid we seek."
-      }, // Ayat End
+        "English Translation": "Thee do we worship, and Thine aid we seek"
+      },
       {
         "Ayat": "اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ",
         "Bangla Translation": "আমাদেরকে সরল পথ দেখাও,",
-        "English Translation": "Show us the straight way,"
-      }, // Ayat End
+        "English Translation": "Show us the straight way"
+      },
       {
         "Ayat":
             "صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ                        ",
         "Bangla Translation":
             "সে সমস্ত লোকের পথ, যাদেরকে তুমি নেয়ামত দান করেছ। তাদের পথ নয়, যাদের প্রতি তোমার গজব নাযিল হয়েছে এবং যারা পথভ্রষ্ট হয়েছে।",
         "English Translation":
-            "The way of those on whom Thou hast bestowed Thy Grace, those whose (portion) is not wrath, and who go not astray."
-      }, // Ayat End
+            "The way of those on whom Thou hast bestowed Thy Grace, those whose (portion) is not wrath, and who go not astray"
+      },
     ]
-  }, // Surah End
+  },
   {
     "Surah No": 108,
     "Name": "সূরা কাওসার",
@@ -57,25 +59,25 @@ List quranData = [
         "Ayat": "إِنَّا أَعْطَيْنَاكَ الْكَوْثَرَ",
         "Bangla Translation": "নিশ্চয় আমি আপনাকে কাওসার দান করেছি।",
         "English Translation":
-            "To thee have We granted the Fount (of Abundance).",
-      }, //Ayat end
+            "To thee have We granted the Fount (of Abundance)",
+      },
       {
         "Ayat No": 2,
         "Ayat": "فَصَلِّ لِرَبِّكَ وَانْحَرْ",
         "Bangla Translation":
-            "অতএব আপনার পালনকর্তার উদ্দেশ্যে নামায পড়ুন এবং কোরবানী করুন।",
+            "তাই আপনি আপনার পালনকর্তার জন্য নামায আদায় করুন এবং কুরবানী করুন।",
         "English Translation":
-            "Therefore to thy Lord turn in Prayer and Sacrifice.",
-      }, //Ayat end
+            "Therefore to thy Lord turn in Prayer and Sacrifice",
+      },
       {
         "Ayat No": 3,
         "Ayat": "إِنَّ شَانِئَكَ هُوَ الْأَبْتَرُ",
-        "Bangla Translation": "যে আপনার শত্রু, সেই তো লেজকাটা, নির্বংশ।",
+        "Bangla Translation": "যে ব্যক্তি আপনার শত্রু, সে নিশ্চয় অপরাধী।",
         "English Translation":
-            "For he who hateth thee, he will be cut off (from Future Hope).",
-      }, //Ayat end
+            "For he who hateth thee, he will be cut off (from Future Hope)",
+      },
     ],
-  }, //Surah End
+  },
   {
     "Surah No": 109,
     "Name": "সূরা কাফিরুন",
@@ -83,43 +85,43 @@ List quranData = [
       {
         "Ayat No": 1,
         "Ayat": "قُلْ يَا أَيُّهَا الْكَافِرُونَ",
-        "Bangla Translation": "বলুন, হে কাফেরকূল,",
+        "Bangla Translation": "বলুন, হে কাফেরগণ,",
         "English Translation": "Say : O ye that reject Faith!",
-      }, //Ayat end
+      },
       {
         "Ayat No": 2,
         "Ayat": "لَا أَعْبُدُ مَا تَعْبُدُونَ",
-        "Bangla Translation": "আমি এবাদত করিনা, তোমরা যার এবাদত কর।",
-        "English Translation": "I worship not that which you worship,",
-      }, //Ayat end
+        "Bangla Translation": "আমি তা পূজা করি না যা তোমরা পূজা কর।",
+        "English Translation": "I worship not that which ye worship",
+      },
       {
         "Ayat No": 3,
         "Ayat": "وَلَا أَنتُمْ عَابِدُونَ مَا أَعْبُدُ",
-        "Bangla Translation": "এবং তোমরাও এবাদতকারী নও, যার এবাদত আমি করি",
-        "English Translation": "Nor will ye worship that which I worship.",
-      }, //Ayat end
+        "Bangla Translation": "আর তোমরা আমি যা পূজা করি তা পূজা করো না।",
+        "English Translation": "Nor will ye worship that which I worship",
+      },
       {
         "Ayat No": 4,
         "Ayat": "وَلَا أَنَا عَابِدٌ مَّا عَبَدتُّمْ",
-        "Bangla Translation": "এবং আমি এবাদতকারী নই, যার এবাদত তোমরা কর।",
+        "Bangla Translation": "আমি তা পূজা করি না যা তোমরা পূজা কর।",
         "English Translation":
             "And I will not worship that which ye have been wont to worship,",
-      }, //Ayat end
+      },
       {
         "Ayat No": 5,
         "Ayat": "وَلَا أَنتُمْ عَابِدُونَ مَا أَعْبُدُ",
-        "Bangla Translation": "তোমরা এবাদতকারী নও, যার এবাদত আমি করি।",
-        "English Translation": "Nor will ye worship that which I worship.",
-      }, //Ayat end
+        "Bangla Translation": "আর তোমরা আমি যা পূজা করি তা পূজা করো না।",
+        "English Translation": "Nor will ye worship that which I worship",
+      },
       {
         "Ayat No": 6,
         "Ayat": "لَكُمْ دِينُكُمْ وَلِيَ دِينِ",
         "Bangla Translation":
-            "তোমাদের কর্ম ও কর্মফল তোমাদের জন্যে এবং আমার কর্ম ও কর্মফল আমার জন্যে।",
-        "English Translation": "To you be your Way, and to me mine.",
-      }, //Ayat end
+            "তোমাদের ধর্ম তোমাদের জন্য আর আমার ধর্ম আমার জন্য।",
+        "English Translation": "To you be your Way, and to me mine",
+      },
     ],
-  }, //Surah End
+  },
   {
     "Surah No": 110,
     "Name": "সূরা নছর",
@@ -127,28 +129,28 @@ List quranData = [
       {
         "Ayat No": 1,
         "Ayat": "إِذَا جَاءَ نَصْرُ اللَّهِ وَالْفَتْحُ",
-        "Bangla Translation": "যখন আসবে আল্লাহর সাহায্য ও বিজয়",
-        "English Translation": "When comes the Help of Allah, and Victory,",
-      }, //Ayat end
+        "Bangla Translation": "যখন আল্লাহর সাহায্য ও বিজয় আসে,",
+        "English Translation": "When comes the Help of Allah, and Victory",
+      },
       {
         "Ayat No": 2,
         "Ayat": "وَرَأَيْتَ النَّاسَ يَدْخُلُونَ فِي دِينِ اللَّهِ أَفْوَاجًا",
         "Bangla Translation":
-            "এবং আপনি মানুষকে দলে দলে আল্লাহর দ্বীনে প্রবেশ করতে দেখবেন,",
+            "এবং তুমি দেখবে মানুষ আল্লাহর ধর্মে গোষ্ঠীগত ভাবে প্রবেশ করছে,",
         "English Translation":
-            "And thou dost see the people enter Allah.s Religion in crowds,",
-      }, //Ayat end
+            "And thou dost see the people enter Allah's Religion in crowds",
+      },
       {
         "Ayat No": 3,
         "Ayat":
-            "فَسَبِّحْ بِحَمْدِ رَبِّكَ وَاسْتَغْفِرْهُ ۚ إِنَّهُ كَانَ تَوَّابًا",
+            "فَسَبِّحْ بِحَمْدِ رَبِّكَ وَاسْتَغْفِرْهُ إِنَّهُ كَانَ تَوَّابًا",
         "Bangla Translation":
-            "তখন আপনি আপনার পালনকর্তার পবিত্রতা বর্ণনা করুন এবং তাঁর কাছে ক্ষমা প্রার্থনা করুন। নিশ্চয় তিনি ক্ষমাকারী।",
+            "তাহলে তুমি তোমার পালনকর্তার প্রশংসা কর এবং তাঁর ক্ষমা প্রার্থনা কর, নিশ্চয় তিনি ক্ষমাশীল।",
         "English Translation":
-            "Celebrate the praises of thy Lord, and pray for His Forgiveness: For He is Oft-Returning (in Grace and Mercy).",
-      }, //Ayat end
+            "Celebrate the praises of thy Lord, and pray for His Forgiveness: For He is Oft-Returning (in Grace and Mercy)",
+      }
     ],
-  }, //Surah End
+  },
   {
     "Surah No": 111,
     "Name": "সূরা লাহাব",
@@ -156,42 +158,41 @@ List quranData = [
       {
         "Ayat No": 1,
         "Ayat": "تَبَّتْ يَدَا أَبِي لَهَبٍ وَتَبَّ",
-        "Bangla Translation":
-            "আবু লাহাবের হস্তদ্বয় ধ্বংস হোক এবং ধ্বংস হোক সে নিজে,",
+        "Bangla Translation": "পেরে যাক আবু লাহাবের হাত ও পেরে যাক আবু লাহাব।",
         "English Translation":
-            "Perish the hands of the Father of Flame! Perish he!",
-      }, //Ayat end
+            "Perish the hands of the Father of Flame! Perish he",
+      },
       {
         "Ayat No": 2,
         "Ayat": "مَا أَغْنَىٰ عَنْهُ مَالُهُ وَمَا كَسَبَ",
         "Bangla Translation":
-            "কোন কাজে আসেনি তার ধন-সম্পদ ও যা সে উপার্জন করেছে।",
+            "তাঁর ধন তাঁকে কিছুই লাভ করে না এবং তাঁর কাজের কিছুই লাভ করে না।",
         "English Translation":
-            "No profit to him from all his wealth, and all his gains!",
-      }, //Ayat end
+            "No profit to him from all his wealth, and all his gains",
+      },
       {
         "Ayat No": 3,
         "Ayat": "سَيَصْلَىٰ نَارًا ذَاتَ لَهَبٍ",
-        "Bangla Translation": "সত্বরই সে প্রবেশ করবে লেলিহান অগ্নিতে",
+        "Bangla Translation": "তিনি নিশ্চয় একটি লোহার জ্বালানির জন্য যাবেন।",
         "English Translation":
-            "Burnt soon will he be in a Fire of Blazing Flame!",
-      }, //Ayat end
+            "Burnt soon will he be in a Fire of Blazing Flame",
+      },
       {
         "Ayat No": 4,
         "Ayat": "وَامْرَأَتُهُ حَمَّالَةَ الْحَطَبِ",
-        "Bangla Translation": "এবং তার স্ত্রীও-যে ইন্ধন বহন করে,",
+        "Bangla Translation": "এবং তাঁর স্ত্রী তাঁর কাছে কাঠের টোকা নিয়ে আসে।",
         "English Translation":
-            "His wife shall carry the (crackling) wood - As fuel!-",
-      }, //Ayat end
+            "His wife shall carry the (crackling) wood - As fuel",
+      },
       {
         "Ayat No": 5,
         "Ayat": "فِي جِيدِهَا حَبْلٌ مِّن مَّسَدٍ",
-        "Bangla Translation": "তার গলদেশে খর্জুরের রশি নিয়ে।",
+        "Bangla Translation": "তাঁর গলার চারপাশে একটি বাঁধা থাকে।",
         "English Translation":
-            "A twisted rope of palm-leaf fibre round her (own) neck!",
-      }, //Ayat end
+            "A twisted rope of palm-leaf fibre round her (own) neck",
+      },
     ],
-  }, //Surah End
+  },
   {
     "Surah No": 112,
     "Name": "সূরা এখলাছ",
@@ -199,29 +200,29 @@ List quranData = [
       {
         "Ayat No": 1,
         "Ayat": "قُلْ هُوَ اللَّهُ أَحَدٌ",
-        "Bangla Translation": "বলুন, তিনি আল্লাহ, এক,",
-        "English Translation": "Say: He is Allah, the One and Only;",
-      }, //Ayat end
+        "Bangla Translation": "বলুন, তিনি আল্লাহ, একমাত্র আল্লাহ।",
+        "English Translation": "Say: He is Allah, the One and Only",
+      },
       {
         "Ayat No": 2,
         "Ayat": "اللَّهُ الصَّمَدُ",
-        "Bangla Translation": "আল্লাহ অমুখাপেক্ষী,",
-        "English Translation": "Allah, the Eternal, Absolute;",
-      }, //Ayat end
+        "Bangla Translation": "আল্লাহ নিরাপদ,",
+        "English Translation": "Allah, the Eternal, Absolute",
+      },
       {
         "Ayat No": 3,
         "Ayat": "لَمْ يَلِدْ وَلَمْ يُولَدْ",
-        "Bangla Translation": "তিনি কাউকে জন্ম দেননি এবং কেউ তাকে জন্ম দেয়নি",
+        "Bangla Translation": "তিনি কাউকে জন্ম দেননি এবং তাঁকে কেউ জন্ম দেয়নি।",
         "English Translation": "He begetteth not, nor is He begotten",
-      }, //Ayat end
+      },
       {
         "Ayat No": 4,
         "Ayat": "وَلَمْ يَكُن لَّهُ كُفُوًا أَحَدٌ",
-        "Bangla Translation": "এবং তার সমতুল্য কেউ নেই।",
-        "English Translation": "And there is none like unto Him.",
-      }, //Ayat end
+        "Bangla Translation": "এবং তাঁর কোনো সমকক্ষ নেই।",
+        "English Translation": "And there is none like unto Him",
+      },
     ],
-  }, //Surah End
+  },
   {
     "Surah No": 113,
     "Name": "সূরা ফালাক্ব",
